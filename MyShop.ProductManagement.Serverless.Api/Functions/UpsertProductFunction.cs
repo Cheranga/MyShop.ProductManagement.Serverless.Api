0b41821992d0d4b7a1db7b0542231af01fc68327
Cheranga Hatangala <cheranga@gmail.com>
2021-01-09 23:27:33 +1100

    Add project files.

---
+++ MyShop.ProductManagement.Serverless.Api/Functions/UpsertProductFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MyShop.ProductManagement.Serverless.Api.Functions
+{
+    public class UpsertProductFunction
+    {
+        public UpsertProductFunction(ILogger<UpsertProductFunction> logger)
+        {
+            
+        }
+
+        [FunctionName(nameof(UpsertProductFunction))]
+        public async Task<IActionResult> UpsertProductAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "products")]
+            HttpRequest request)
+        {
+            var content = await new StreamReader(request.Body).ReadToEndAsync();
+            
+            return new OkResult();
+        }
+    }
+}
---
+++ MyShop.ProductManagement.Serverless.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using MyShop.ProductManagement.Serverless.Api;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace MyShop.ProductManagement.Serverless.Api
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //
+            // TODO: register dependencies
+            //
+        }
+    }
+}
