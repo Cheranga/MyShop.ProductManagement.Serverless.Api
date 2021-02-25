b4819f2d9851e16735963b7233eea1bf2fea5296
Cheranga Hatangala <cheranga@gmail.com>
2021-02-25 17:13:39 +1100

    Adding a test function.

---
+++ MyShop.ProductManagement.Serverless.Api/Functions/TestFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace MyShop.ProductManagement.Serverless.Api.Functions
+{
+    public class TestFunction
+    {
+        [FunctionName(nameof(TestFunction))]
+        public Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethod.Get), Route = "test")]
+            HttpRequest request)
+        {
+            var actionResult = (IActionResult) (new OkResult());
+            return Task.FromResult(actionResult);
+        }
+    }
+}
